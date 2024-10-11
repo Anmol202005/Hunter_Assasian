9fdfa4b7cb9814772b1e416cf9b959715d26ba7f
tanvee30 <tanvee2331155@akgec.ac.in>
2024-10-11 15:44:14 +0530

    added map layout of game

---
+++ map.js
@@ -0,0 +1,18 @@
+ export class Boundary{
+    static width=50;
+    static height=50;
+
+    constructor({position,image}){
+        this.position = position;
+        this.image=image;
+
+    }
+
+    draw(){
+        const canvas=document.querySelector('canvas');
+const c=canvas.getContext('2d');
+        c.drawImage(this.image,this.position.x,this.position.y);
+    }
+   
+    
+}
---
+++ script.js
@@ -0,0 +1,97 @@
+
+
+const canvas=document.querySelector('canvas');
+const c=canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+import {Boundary} from "./map.js";
+
+c.fillStyle = 'gray';
+c.fillRect(0, 0, canvas.width, canvas.height); 
+const map=[
+    ['7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7'],   
+    ['7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7'],   
+    ['7','7','3','0','0','0','0','0','0','0','0','0','0','3','3','3','0','0','0','0','0','7','7'],   
+    ['7','7','0','0','0','1','0','0','0','3','0','0','0','0','3','3','0','0','0','0','4','7','7'], 
+    ['7','7','0','0','0','0','0','0','3','1','3','0','0','0','3','1','3','0','0','0','0','7','7'],   
+    ['7','7','4','0','0','3','0','0','0','0','0','0','0','0','0','3','0','0','0','0','0','7','7'],
+    ['7','7','0','0','0','3','0','0','0','3','0','0','0','0','3','3','0','3','3','3','0','7','7'],
+    ['7','7','0','0','0','3','3','0','3','3','3','0','0','0','0','0','0','3','0','0','0','7','7'],
+    ['7','7','0','3','0','0','0','0','3','3','3','0','0','0','0','0','0','3','0','1','3','7','7'],
+    ['7','7','0','0','0','0','0','0','1','0','0','0','0','3','0','0','2','4','0','0','0','7','7'],
+    ['7','7','2','0','0','0','0','3','3','3','0','0','0','3','3','0','0','0','0','0','0','7','7'],
+    ['7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7'],
+    ['7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7','7'],  
+    
+]
+
+
+const img =new Image();
+img.src="wall.jpg";
+img.onload=()=>{
+map.forEach((row,i)=>{
+    row.forEach((symbol,j)=>{
+        switch(symbol){
+            case '7':new Boundary({position : {x:180+50*j,y:50+50*i},image:img}).draw();}
+                
+                   
+    })
+})
+}
+const bmg1 =new Image();
+bmg1.src="block1.jpg";
+bmg1.onload=()=>{
+map.forEach((row,i)=>{
+    row.forEach((symbol,j)=>{
+        switch(symbol){
+            case '1':new Boundary({position : {x:180+50*j,y:50+50*i},image:bmg1}).draw();}
+                
+                   
+    })
+})
+}
+const bmg2 =new Image();
+bmg2.src="block2.jpg";
+bmg2.onload=()=>{
+map.forEach((row,i)=>{
+    row.forEach((symbol,j)=>{
+        switch(symbol){
+            case '2':new Boundary({position : {x:180+50*j,y:50+50*i},image:bmg2}).draw();}
+                
+                   
+    })
+})
+}
+
+const bmg3 =new Image();
+bmg3.src="block3.jpg";
+bmg3.onload=()=>{
+map.forEach((row,i)=>{
+    row.forEach((symbol,j)=>{
+        switch(symbol){
+            case '3':new Boundary({position : {x:180+50*j,y:50+50*i},image:bmg3}).draw();}
+                
+                   
+    })
+})
+}
+
+const bmg4 =new Image();
+bmg4.src="block4.jpg";
+bmg4.onload=()=>{
+map.forEach((row,i)=>{
+    row.forEach((symbol,j)=>{
+        switch(symbol){
+            case '4':new Boundary({position : {x:180+50*j,y:50+50*i},image:bmg4}).draw();}
+                
+                   
+    })
+})
+}
+
+
+    
+
+
