bc9ebd7778f6ab04d132cc3391e1e8dbd2fca1fd
tanvee30 <tanvee2331155@akgec.ac.in>
2024-10-12 21:29:19 +0530

    updated villain

--- scripts/villain.js
+++ scripts/villain.js
@@ -3,9 +3,9 @@ class Villain {
         this.position = position;
         this.velocity = velocity;
         this.image = image;
-        
-        this.width = 119; 
-        this.height = 98; 
+
+        this.width = 619; 
+        this.height = 403; 
     }
 
     update() {
@@ -20,37 +20,35 @@ class Villain {
         const canvas = document.querySelector(".map");
         const c = canvas.getContext("2d");
         
-        
-        c.drawImage( this.image, 0,0,this.width,this.height,this.position.x,this.position.y,50,50);
-            
-            
-        
+        c.drawImage(this.image, 0, 0, this.width, this.height, this.position.x, this.position.y, 50, 50);
     }
 }
-
-
-
 
 const canvas = document.querySelector(".map");
 const c = canvas.getContext("2d");
 
-
-        
 const villainImage = new Image();
-villainImage.src = "villainspirite2.png"; 
-let vill;
+villainImage.src = "../images/vill.png"; 
+
+let villains = [];
+
+
 villainImage.onload = () => {
-     vill = new Villain({ position: { x: 800, y: 200 }, velocity: { x: 0, y: 0 }, image: villainImage });
+    villains.push(new Villain({ position: { x: 800, y: 200 }, velocity: { x: 0, y: 0 }, image: villainImage }));
+    villains.push(new Villain({ position: { x: 900, y: 500 }, velocity: { x: 0, y: 0 }, image: villainImage }));
+    villains.push(new Villain({ position: { x: 600, y: 300 }, velocity: { x: 0, y: 0 }, image: villainImage }));
 
     function move() {
-        c.clearRect(vill.position.x, vill.position.y, 50, 50); 
-        vill.update(); 
-        vill.build(); 
+        
+        
+
+        villains.forEach(vill => {
+            vill.update(); 
+            vill.build(); 
+        });
+
         requestAnimationFrame(move); 
     }
 
-
-move(); 
+    move(); 
 }
-
-
--- scripts/villainclass.js
+++ scripts/villainclass.js
@@ -1,59 +1,7 @@
-class Villain {
-    constructor({ position, velocity, image }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.image = image;
 
-        this.width = 119;
-        this.height = 98;
-    }
 
-    update() {
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-    }
 
-    build() {
-        const canvas = document.querySelector(".map");
-        const c = canvas.getContext("2d");
 
-        c.drawImage(this.image, 0, 0, this.width, this.height, this.position.x, this.position.y, 50, 50);
-    }
-}
 
-const canvas = document.querySelector(".map");
-const c = canvas.getContext("2d");
-
-const villainImages = [
-    "villainspirite2.png",
-    "villainspirite2.png",  
-    "villainspirite2.png"   
-];
-
-let villains = [];
-
-let loadedImages = 0;
-
-function createVillain(index, x, y) {
-    const villainImage = new Image();
-    villainImage.src = villainImages[index];
-    villainImage.onload = () => {
-        villains.push(new Villain({ position: { x: x, y: y }, velocity: { x: 0, y: 0 }, image: villainImage }));
-        loadedImages++;
-        if (loadedImages === villainImages.length) {
-            move();
-        }
-    };
-}
-
-createVillain(0, 800, 200);  
-createVillain(1, 400, 100);  
-createVillain(2, 600, 300);  
-function move() {
-    c.clearRect(0, 0, canvas.width, canvas.height);  
-    villains.forEach(vill => {
-        vill.update();
-        vill.build();
-    });
-    requestAnimationFrame(move);
-}
+    
+        
